0ab260b94ebbbd53da90e0226ff97cae656765ff
Marcin Lozowski <marcin.lozowski@wp.pl>
2017-05-30 17:30:23 +0200

    ajax URL taken from form attribute from now

--- growtweet/static/js/twitter-auth.js
+++ growtweet/static/js/twitter-auth.js
@@ -1,13 +1,14 @@
 
 function twitter_auth() {
   var form = $('#twitter-form');
+  var url = form.attr('data-validate-username-url');
   $.ajax({
-    url: '/',
+    url: url,
     type: 'POST',
     data: form.serialize(),
     dataType: 'json',
     success: function (data) {
-        alert(data);
+
     }
   });
 }

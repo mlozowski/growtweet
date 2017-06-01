c4c4db0a916146867791d4550e581b3cab070fde
Marcin Lozowski <marcin.lozowski@wp.pl>
2017-06-01 17:15:50 +0200

    Added 'twitter' app in settings and urls

--- growtweet/urls.py
+++ growtweet/urls.py
@@ -1,8 +1,9 @@
 
-from django.conf.urls import url
+from django.conf.urls import url, include
 
 from growtweet import views
 
 urlpatterns = [
     url(r'^$', views.Main.as_view(), name='main-page'),
+    url(r'^', include('twitter.urls')),
 ]

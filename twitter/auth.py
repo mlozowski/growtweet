8f8762fd27ea97dcd64fa2bba423b73724a9401e
Marcin Lozowski <marcin.lozowski@wp.pl>
2017-06-05 16:19:22 +0200

    created config.py module in twitter package

--- twitter/auth.py
+++ twitter/auth.py
@@ -1,27 +1,25 @@
 
-import os
-import configparser
 import oauth2
 from urllib.parse import parse_qsl
 
 from django.shortcuts import redirect
 
+from .config import (
+    CONSUMER_KEY,
+    CONSUMER_SECRET,
+    REQUEST_TOKEN_ADDRESS,
+    TWITTER_LOGIN_ADDRESS,
+)
+
 
 def request_token():
-    # Set the consumer key and secret
-    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    config = configparser.ConfigParser()
-    config.read(os.path.join(BASE_DIR, 'config.ini'))
-    CONSUMER_KEY = config['oauth']['consumer-key']
-    CONSUMER_SECRET = config['oauth']['consumer-secret']
-
     # Create a oauth2.Consumer object that wraps the parameters
     # for the calls to the HTTP endpoints
     consumer = oauth2.Consumer(CONSUMER_KEY, CONSUMER_SECRET)
 
     # Use the oauth2.Client class to call the Twitter OAuth endpoint
     resp, content = oauth2.Client(consumer).request(
-        'https://api.twitter.com/oauth/request_token', "GET")
+        REQUEST_TOKEN_ADDRESS, "GET")
 
     # Create a standard dictionary from the response body, using parse_qsl as
     # a convenience to parse the query string in the response
@@ -32,7 +30,7 @@ def redirect_to_twitter_login_page(token_data_dir):
     # Redirect user to authorization page, encapsulating request token in URL
     return redirect(
         "%s?oauth_token=%s" % (
-            'https://api.twitter.com/oauth/authorize',
+            TWITTER_LOGIN_ADDRESS,
             token_data_dir[b'oauth_token'].decode('utf-8')
         )
     )
---
+++ twitter/config.py
@@ -0,0 +1,13 @@
+
+import os
+import configparser
+
+
+# Set the consumer key and secret
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+config = configparser.ConfigParser()
+config.read(os.path.join(BASE_DIR, 'config.ini'))
+CONSUMER_KEY = config['oauth']['consumer-key']
+CONSUMER_SECRET = config['oauth']['consumer-secret']
+REQUEST_TOKEN_ADDRESS = config['oauth']['request-token-address']
+TWITTER_LOGIN_ADDRESS = config['oauth']['twitter-login-address']

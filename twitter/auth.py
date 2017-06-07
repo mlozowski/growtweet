baafda65433c54a7db0ff320a991fd55d49b2284
Marcin Lozowski <marcin.lozowski@wp.pl>
2017-06-07 14:28:01 +0200

    Getting my followers works

--- twitter/auth.py
+++ twitter/auth.py
@@ -1,61 +1,26 @@
 
-import oauth2
-from urllib.parse import parse_qsl
+import tweepy
 
 from django.shortcuts import redirect
 
 from .config import (
     CONSUMER_KEY,
     CONSUMER_SECRET,
-    TOKEN_SECRET,
-    REQUEST_TOKEN_ADDRESS,
-    TWITTER_LOGIN_ADDRESS,
-    TWITTER_ACCESS_TOKEN_ADDRESS,
 )
 
 
-def request_token():
-    # Create a oauth2.Consumer object that wraps the parameters
-    # for the calls to the HTTP endpoints
-    consumer = oauth2.Consumer(CONSUMER_KEY, CONSUMER_SECRET)
-
-    # Use the oauth2.Client class to call the Twitter OAuth endpoint
-    resp, content = oauth2.Client(consumer).request(
-        REQUEST_TOKEN_ADDRESS, "GET")
-
-    # Create a standard dictionary from the response body, using parse_qsl as
-    # a convenience to parse the query string in the response
-    return dict(parse_qsl(content))
+def get_twitter_api(request):
+    verifier = request.GET.get('oauth_verifier', None)
+    oauth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    token = request.session['request_token']
+    del request.session['request_token']
+    oauth.request_token = token
+    oauth.get_access_token(verifier)
+    return tweepy.API(oauth)
 
 
-def redirect_to_twitter_login_page(token_data_dir):
-    """
-    Redirect user to authorization page, encapsulating request token in URL
-    :param token_data_dir: dir
-    :return: redirect
-    """
-    return redirect(
-        "%s?oauth_token=%s" % (
-            TWITTER_LOGIN_ADDRESS,
-            token_data_dir[b'oauth_token'].decode('utf-8')
-        )
-    )
-
-
-def access_data(oauth_token, oauth_verifier):
-    """
-    Get access token and other related data in form
-    {"user_id": "2343", "x_auth_expires": "0", "screen_name": "jjsdh", "
-    oauth_token": "HFYFTUT", "oauth_token_secret": "RDYTTYFYTGF"}
-    :param oauth_token: str
-    :param oauth_verifier: str
-    :return: dict
-    """
-    token = oauth2.Token(oauth_token, TOKEN_SECRET)
-    token.set_verifier(oauth_verifier)
-    consumer = oauth2.Consumer(CONSUMER_KEY, CONSUMER_SECRET)
-    client = oauth2.Client(consumer, token)
-    resp, content = client.request(
-        TWITTER_ACCESS_TOKEN_ADDRESS, "POST")
-    access_token = dict(parse_qsl(content.decode('utf-8')))
-    return access_token, resp
+def redirect_to_twitter_auth(request):
+    oauth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    redirect_url = oauth.get_authorization_url()
+    request.session['request_token'] = oauth.request_token
+    return redirect(redirect_url)
--- twitter/views.py
+++ twitter/views.py
@@ -1,26 +1,19 @@
 
-import json
-
 from django.http import HttpResponse
 from django.views.generic import View
 
-from . import auth
-from . import api
+from .auth import (
+    redirect_to_twitter_auth,
+    get_twitter_api,
+)
 
 
 def twitter_login(request):
-    token_data = auth.request_token()
-    return auth.redirect_to_twitter_login_page(token_data)
+    return redirect_to_twitter_auth(request)
 
 
 class Followers(View):
     def get(self, request):
-        oauth_token = request.GET.get('oauth_token', None)
-        oauth_verifier = request.GET.get('oauth_verifier', None)
-        if oauth_token is not None and oauth_verifier is not None:
-            access_data, response = auth.access_data(
-                oauth_token, oauth_verifier)
-            followers_data = api.followers(access_data['oauth_token'])
-            return HttpResponse(json.dumps(followers_data))
-        else:
-            raise NotImplemented
+        api = get_twitter_api(request)
+        followers_data = api.followers()
+        return HttpResponse('jest')

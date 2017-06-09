6cf424fd25d7c6052beec2b15eca7d958eae4549
Marcin Lozowski <marcin.lozowski@wp.pl>
2017-06-09 18:01:59 +0200

    JavaScript code creating table from response data.

--- twitter/static/tweetjs/followers.js
+++ twitter/static/tweetjs/followers.js
@@ -12,10 +12,36 @@ function followers() {
     timeout: 2*60*60*1000,
     dataType: 'json',
     success: function (data) {
-        $('div.loading-field').html('<h1>It works!</h1>');
+        $('div.loading-field').html(create_table(data));
     },
     error: function (request, status, error) {
         $('div.loading-field').html('<h1>An error occurred</h1>');
     }
   });
+}
+
+
+function create_table(data) {
+    var table = "" +
+    "<table border=1>" +
+      "<thead>" +
+        "<tr>" +
+          "<th>Follower of my followers</th>" +
+          "<th>Number of my followers</th>" +
+        "</tr>" +
+      "</thead>" +
+      "<tbody>";
+
+    $.each( data.result, function( key, value ) {
+      table += "<tr>";
+      table += "<td>" + value.follower_follower_name + "</td>";
+      table += "<td>" + value.followers_count + "</td>";
+      table += "</tr>";
+    });
+
+    table += "" +
+      "</tbody>" +
+    "</table>";
+
+    return table;
 }

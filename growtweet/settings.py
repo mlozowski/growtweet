0ca9d773da199d3ea34720587d9c83eaad202e6f
Marcin Lozowski <marcin.lozowski@wp.pl>
2017-06-10 21:21:55 +0200

    Added rest_framework to INSTALLED_APPS. Thanks to that rest_framework static files will be used.

--- growtweet/settings.py
+++ growtweet/settings.py
@@ -27,6 +27,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # external packages
+    'rest_framework',
+
     # my apps
     'growtweet',
     'twitter',

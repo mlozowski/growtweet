9e9c8ce2b41a6d43b254ec033f140085c44e979d
Marcin Lozowski <marcin.lozowski@wp.pl>
2017-06-11 18:23:07 +0200

    Cleaning not needed files and parameters

--- growtweet/views.py
+++ growtweet/views.py
@@ -1,16 +1,6 @@
 
 from django.views.generic import FormView
-from django.http import JsonResponse
-
-from growtweet.forms import TwitterUserForm
 
 
 class Main(FormView):
     template_name = "growtweet/main.html"
-    form_class = TwitterUserForm
-
-    def form_valid(self, form):
-        return JsonResponse({'status': 'OK'})
-
-    def form_invalid(self, form):
-        return JsonResponse(form.errors, status=400)
